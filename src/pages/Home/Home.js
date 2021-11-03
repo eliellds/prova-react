9655b8b413604ecc775d5ec19b3f9ab6a9a62e4e
Eliel Lopes <85817324+eliellds@users.noreply.github.com>
2021-11-03 12:40:12 -0300

    ajustando css de header

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home(props) {
+
+    return (
+        <>
+            <h1>Home</h1>
+        </>
+    )
+
+}
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,14 @@
 import React from "react"
-import { Switch, Route} from "react-router-dom"
+import { Switch, Route } from "react-router-dom"
+
+import Home from "./pages/Home/Home"
 
 export const Routes = () => {
 
     return (
         <Switch>
-            
+            <Route path="/" component={Home} exact/>
+            <Route path="/home" component={Home}/>
         </Switch>
     )
 
