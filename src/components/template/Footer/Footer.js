d39a9c5eeaaae937faa429c5319a35abfb74d7de
Eliel Lopes <85817324+eliellds@users.noreply.github.com>
2021-11-03 11:40:15 -0300

    implementando header e footer e adicionando seus respectivos css

---
+++ src/components/template/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './Header.css'
+
+export default function Footer(props) {
+
+    return (
+        <>
+            <footer class="dashboard">
+                <p>&copy 2020 Collective. All Rights Reserved | Design by <a href="https://w3layouts.com/" target="_blank"
+                    class="text-primary">W3layouts.</a></p>
+            </footer>
+        </>
+    )
+
+}
+
